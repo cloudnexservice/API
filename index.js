2f1df687fb17ab45938d9456c78e141cff3bc5a1
IntrustlabsIIL <ayeshasahani1303@gmail.com>
2026-02-12 13:18:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log(`Server is running on port ${PORT}`)
+)
+
+app.get('/user', (req, res) => {
+    res.status(200).send({
+        op: 'Success'
+    })
+})
