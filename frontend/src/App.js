6090e772d67e1358ed07eab6f7dec7ebe9bdb8b9
IntrustlabsIIL <ayeshasahani1303@gmail.com>
2026-02-12 18:50:56 +0530

    fix frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const[data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://54.242.213.243:8080/user")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("API Response:", data);
+        setData(data);
+      })
+      .catch(err=> console.error("Error fetching data:", err));
+  }, []);
+
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1> API Response</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+  );
+}
+
+export default App;
