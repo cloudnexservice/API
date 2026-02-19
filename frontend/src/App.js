e0447b3d79e32a8175c370b6196332fe4801f438
IntrustlabsIIL <ayeshasahani1303@gmail.com>
2026-02-19 14:13:06 +0530

    url changed in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const[data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("https://api-production-a8ec.up.railway.app/users")
+    fetch("https://api-production-a8ec.up.railway.app/user")
       .then((res) => res.json())
       .then((data) => {
         console.log("API Response:", data);
         setData(data);
       })
-      .catch(err=> console.error("Error fetching data:", err));
+      .catch((err) => console.error("Error fetching data:", err));
   }, []);
 
   return (
-    <div style={{textAlign: "center", marginTop: "50px"}}>
-      <h1> API Response</h1>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>API Response</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+    </div>
   );
 }
 
